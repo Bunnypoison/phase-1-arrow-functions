0f9f3cc40b3a4d7436ec26fd4e7e18b98b5070c3
Erica Domerese <xrabbitonthemoonx@gmail.com>
2024-07-19 12:46:42 -0500

    phase-1-arrow-functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function called square
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  // Test the functions
+  console.log(divide()); // Output: 20
+  console.log(square(5)); // Output: 25
+  console.log(add(3, 7)); // Output: 10
